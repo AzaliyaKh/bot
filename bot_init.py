6de587d4b8810b524a7c27f0e381b0070aa9aedb
Azaliya <a_khakimyanova@mail.ru>
2025-04-21 17:15:29 +0500

    initialization

---
+++ bot_init.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher
+import logging
+
+API_TOKEN = '7969649315:AAFSUlkFX9-RBT0s-c_EW8B9yLcc-Yizl-w'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+logger = logging.getLogger(__name__)
